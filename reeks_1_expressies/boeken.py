537411341d695b9fc919d78bda99dda3fea16ad3
Michaël Vandenvelde <michael.vandenvelde@student.hogent.be>
2026-09-23 22:18:27 +0200

    eerste oefening opgelost

---
+++ reeks_1_expressies/boeken.py
@@ -0,0 +1,5 @@
+prijs_boek = 24.95
+prijs_boek_winkel = prijs_boek - (prijs_boek * 0.40)
+prijs_versturen = 3 + (59 * 0.75)
+totale_prijs = (prijs_boek_winkel  *60) + prijs_versturen
+print(totale_prijs)
